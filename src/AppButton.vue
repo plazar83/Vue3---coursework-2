a5f8e727156961f873ecbfff7bf1dfdf1abb513d
Nazar Plakhotnyuk <n_arctic@ukr.net>
2021-01-14 11:55:26 +0200

    create comments component

---
+++ src/AppButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class="btn" :class="btnColor" @click="$emit('action')"><slot /></button>
+</template>
+<script>
+  export default {
+    name: 'AppButton',
+    emits: ['action'],
+    props: {
+      btnColor: {
+        type: String,
+        default: '',
+        validator: function(value) {
+          return ['', 'primary', 'danger'].includes(value)
+        }
+      }
+    }
+  }
+</script>
---
+++ src/AppComments.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-if="comments" class="card">
+        <h2>{{title}}</h2>
+        <ul class="list">
+            <li class="list-item" v-for="comment in comments" :key="comment.id">
+                <div>
+                    <p><strong>{{comment.email}}</strong></p>
+                    <small>{{comment.body}}</small>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: ['comments', 'loading'],
+    data() {
+      return {
+        title: 'Комментарии'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+   .list-item + li {
+       border-top: 1px solid #ccc;
+   }
+</style>

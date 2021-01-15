adb1e836fe38082ee5e905fe87def5d3d6ab9cea
Nazar Plakhotnyuk <n_arctic@ukr.net>
2021-01-15 21:31:39 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,7 @@
     <AppResumeBlocks :blocks="blocks"/>
   </div>
   <div class="container">
-    <p>
-      <AppButton btnColor="primary" @action="loadComments">Загрузить комментарии</AppButton>
-    </p>
-    <AppLoader v-if="loading"></AppLoader>
-    <AppComments v-else @action="loadComments" :loading="loading" :comments="comments" />
+    <AppComments />
   </div>
 </template>
 
@@ -16,35 +12,19 @@
 import AppResumeForm from './AppResumeForm'
 import AppResumeBlocks from './AppResumeBlocks'
 import AppComments from './AppComments'
-import AppButton from './AppButton'
-import AppLoader from './AppLoader'
+
 export default {
   data() {
     return {
-      blocks: [],
-      comments: null,
-      loading: false
+      blocks: []
     }
   },
   methods: {
     addBlock(e) {
       this.blocks.push({type: e.type, value: e.value})
     },
-    async loadComments() {
-      this.loading = true
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/comments?_limit=42')
-        const data = await response.json()
-        this.comments = Object.keys(data).map(key => {
-          return { ...data[key], id: key }
-        })
-      } catch (e) {
-        console.log(e)
-      }
-      this.loading = false
-    }
   },
-  components: {AppResumeForm, AppResumeBlocks, AppComments, AppButton, AppLoader}
+  components: {AppResumeForm, AppResumeBlocks, AppComments}
 }
 </script>
 
--- src/AppComments.vue
+++ src/AppComments.vue
@@ -1,5 +1,9 @@
 <template>
-    <div v-if="comments" class="card">
+    <p>
+        <AppButton btnColor="primary" @action="loadComments">Загрузить комментарии</AppButton>
+    </p>
+    <AppLoader v-if="loading"></AppLoader>
+    <div v-else-if="comments" class="card">
         <h2>{{title}}</h2>
         <ul class="list">
             <li class="list-item" v-for="comment in comments" :key="comment.id">
@@ -13,13 +17,32 @@
 </template>
 
 <script>
+  import AppButton from './AppButton'
+  import AppLoader from './AppLoader'
   export default {
-    props: ['comments'],
     data() {
       return {
-        title: 'Комментарии'
+        title: 'Комментарии',
+        comments: null,
+        loading: false
       }
-    }
+    },
+    methods: {
+      async loadComments() {
+        this.loading = true
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/comments?_limit=42')
+          const data = await response.json()
+          this.comments = Object.keys(data).map(key => {
+            return { ...data[key], id: key }
+          })
+        } catch (e) {
+          console.log(e)
+        }
+        this.loading = false
+      }
+    },
+    components: {AppLoader, AppButton}
   }
 </script>
 
